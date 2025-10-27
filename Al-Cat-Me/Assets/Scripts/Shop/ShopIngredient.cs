26ea16782b9b94bfd241ad498c431976d8cdb007
Alex <alexandrehcayer@gmail.com>
2025-10-26 20:31:45 -0700

    added ingredients to game controller, progress on shop, WIP

--- Al-Cat-Me/Assets/Scripts/Shop/ShopIngredient.cs
+++ Al-Cat-Me/Assets/Scripts/Shop/ShopIngredient.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class ShopIngredient : MonoBehaviour
 {
+    private GameController gameController;
     public ShopManager shopManager;
     public GameObject ingredientText;
-    public Ingredient ingredient;
+    private Ingredient ingredient;
+    private int price;
     private bool available;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
         available = true;
+
+        ingredient = gameController.getRandomIngredient();
+        price = ingredientPrice();
+        Debug.Log("ingredient name string: " + ingredient.GetName());
+        ingredientText.GetComponent<TextMeshProUGUI>().SetText(ingredient.GetName() + "\n" + price + " Gold");
     }
 
     // Update is called once per frame
@@ -22,10 +31,18 @@ public class ShopIngredient : MonoBehaviour
     public void OnButtonClick()
     {
         //replace attemptPurchase amount to ingredient cost
-        if (available && shopManager.attemptPurchase(1))
+        if (available && shopManager.attemptPurchase(price, ingredient))
         {
             ingredientText.GetComponent<TextMeshProUGUI>().SetText("<color=RED>SOLD OUT</color>");
             available = false;
         }
+    }
+
+    private int ingredientPrice()
+    {
+        List<int> stats = new List<int>(ingredient.GetStats());
+        int price = 0;
+        stats.ForEach(stat => price += stat);
+        return price;
     }
 }
--- Al-Cat-Me/Assets/Scripts/Shop/ShopManager.cs
+++ Al-Cat-Me/Assets/Scripts/Shop/ShopManager.cs
@@ -1,38 +1,76 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 using TMPro;
 
 public class ShopManager : MonoBehaviour
 {
     private GameController gameController;
     public GameObject yourGoldText;
+    public GameObject shopkeeperText;
+    public GameObject shopkeeperImage;
+    private readonly float ANIM_TIME = 4;
+    private float animTimer;
+    private bool isDefault;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
         yourGoldText.GetComponent<TextMeshProUGUI>().SetText("Your Gold: " + gameController.getGold());
+        shopkeeperText.GetComponent<TextMeshProUGUI>().SetText("Buy something, will ya?");
+        shopkeeperImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("shopkeeper/Shopkeeper_Fox");
+        animTimer = 0f;
+        isDefault = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!isDefault) animTimer += Time.deltaTime;
+        if (animTimer >= ANIM_TIME) setDefault();
     }
 
-    public bool attemptPurchase(int amount)
+    public bool attemptPurchase(int amount, Ingredient ingredient)
     {
         var playerGold = gameController.getGold();
         if(amount > playerGold)
         {
             //insufficient funds, do nothing
+            shopkeeperText.GetComponent<TextMeshProUGUI>().SetText("You trynna' rip me off, son?");
+            //maybe update shopkeeper animation
+            setAngy();
             return false;
         }
         else
         {
+            List<Ingredient> ingredientAsList = new List<Ingredient> { ingredient };
+            gameController.addIngredients(ingredientAsList);
             gameController.addGold(-amount);
-            //gameController.addIngredients();
             yourGoldText.GetComponent<TextMeshProUGUI>().SetText("Your Gold: " + gameController.getGold());
+            setHapp();
             return true;
         }
+    }
+
+    void setDefault()
+    {
+        shopkeeperImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("shopkeeper/Shopkeeper_Fox");
+        animTimer = 0f;
+        shopkeeperText.GetComponent<TextMeshProUGUI>().SetText("Buy something, will ya?");
+        isDefault = true;
+    }
+
+    void setAngy()
+    {
+        shopkeeperImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("shopkeeper/Shopkeeper_Fox_Angy");
+        isDefault = false;
+    }
+
+    void setHapp()
+    {
+        shopkeeperImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("shopkeeper/Shopkeeper_Fox_Happy");
+        shopkeeperText.GetComponent<TextMeshProUGUI>().SetText("Well now, thank you kindly!");
+        isDefault = false;
     }
 }
